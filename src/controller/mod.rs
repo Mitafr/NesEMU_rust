c77f22accc87e62e2f9f5f56b38bcfe31db79f73
Mitafr <martin.pelletier78@hotmail.com>
2019-12-31 14:11:52 +0100

    fix controller lag

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -48,14 +48,61 @@ impl Controller {
     pub fn poll_events(&mut self, event: &Event) {
         match event {
             Event::KeyDown { keycode: Some(Keycode::A), ..} => self.key = KeyStatus::A,
-            Event::KeyDown { keycode: Some(Keycode::B), ..} => self.key = KeyStatus::B,
-            Event::KeyDown { keycode: Some(Keycode::C), ..} => self.key = KeyStatus::Select,
+            Event::KeyDown { keycode: Some(Keycode::Z), ..} => self.key = KeyStatus::B,
+            Event::KeyDown { keycode: Some(Keycode::Space), ..} => self.key = KeyStatus::Select,
             Event::KeyDown { keycode: Some(Keycode::Return), ..} => self.key = KeyStatus::Start,
             Event::KeyDown { keycode: Some(Keycode::Up), ..} => self.key = KeyStatus::Up,
             Event::KeyDown { keycode: Some(Keycode::Down), ..} => self.key = KeyStatus::Down,
             Event::KeyDown { keycode: Some(Keycode::Left), ..} => self.key = KeyStatus::Left,
             Event::KeyDown { keycode: Some(Keycode::Right), ..} => self.key = KeyStatus::Right,
-            _ => {self.key = KeyStatus::Idle}
+            Event::KeyUp { keycode: Some(Keycode::A), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::Z), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::Space), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::D), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::Up), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::Down), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::Left), ..} => self.key = KeyStatus::Idle,
+            Event::KeyUp { keycode: Some(Keycode::Right), ..} => self.key = KeyStatus::Idle,
+            _ => {}
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Controller;
+    use super::KeyStatus;
+
+    #[test]
+    fn read_without_strobe_should_shift_right() {
+        let mut controller = Controller::new();
+        controller.addr = KeyStatus::B as u8;
+        controller.strobe = false;
+        controller.read();
+        assert_eq!(controller.addr, KeyStatus::B as u8 >> 1);
+    }
+    #[test]
+    fn read_with_strobe_should_return_keycode() {
+        let mut controller = Controller::new();
+        controller.key = KeyStatus::A;
+        controller.strobe = true;
+        controller.read();
+        assert_eq!(controller.addr, 1);
+        controller.key = KeyStatus::Up;
+        controller.strobe = true;
+        controller.read();
+        assert_eq!(controller.addr, KeyStatus::Up as u8);
+    }
+    #[test]
+    fn write_should_strobe() {
+        let mut controller = Controller::new();
+        controller.write(0x1);
+        assert!(controller.strobe);
+    }
+    #[test]
+    fn write_shouldnt_strobe() {
+        let mut controller = Controller::new();
+        controller.write(0xc0);
+        assert!(!controller.strobe);
     }
 }
